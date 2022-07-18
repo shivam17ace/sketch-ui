399aa701dd58203bd9b634c588df9c04469745f8
shivam <shivam17101@gmail.com>
2022-07-18 13:13:37 +0530

    Header

--- src/Components/Welcome/welcome.js
+++ src/Components/Welcome/welcome.js
@@ -1,8 +1,27 @@
 import React from "react";
+import "../Welcome/welcome.scss"
+import Logo from "../Images/logo.png";
+import {MdShoppingBasket} from "react-icons/md";
 function Welcome () {
     return(
-        <div>
+    <div className="container">
+        <div className="navbar">
+            <div className="logo">
+                <img src={Logo} alt="logo" />
+                <span>City</span>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>About Us</li>
+                <li>Service</li>
+            </ul>
+            <span className="icon-shopping">
+                <MdShoppingBasket />
+                <div className="cart_count">1</div>
+            </span>
         </div>
+    </div>
     )
 }
 export default Welcome;
