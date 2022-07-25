64f8580561238b5b5bb33d326ecf36cbeea97959
shivam <shivam17101@gmail.com>
2022-07-25 17:04:49 +0530

    orderpage commit

---
+++ src/Components/CreateOrder/SketchOrderType/PencilSketch.js
@@ -0,0 +1,103 @@
+import React, {useState} from "react";
+import Sketch from "../../../Images/sketch4.png";
+import Button from "@mui/material/Button";
+import {Link} from "react-router-dom";
+function PencilSketch() {
+    const [selectedImage, setSelectedImage] = useState(null);
+        return(
+            <>
+            <div className="tab_content">
+                <div className="top_part">
+                    <div className="sketch_left">
+                        <span className="heading_sketch">Handmade Pencil Sketch</span>
+                        <div>
+                            <img src={Sketch} alt="colorsketch" className="tab_image" />
+                        </div>
+                    </div>
+                    <div className="sketch_detail">
+                        <div>
+                            Size:  A4(8X12 inch), A3(12X16 inch), A2(16X24 inch)
+                        </div>
+                        <div>
+                            Medium:  Pencil Sketch
+                        </div>
+                        <div>
+                            Shipping:  Dehradun Only
+                        </div>
+                        <div>
+                            Framing: Framed and unframed
+                        </div>
+                        <div>
+                            Style: face to shoulder, not full body
+                        </div>
+                    </div>
+                </div>
+                <div className="select_req">
+                    Select your Requirements from Following:
+                    <label>Location:</label>
+                    <select className="client_select">
+                        <option>Choose any Option</option>
+                        <option>Dehradun</option>
+                        <option>Bangalore</option>
+                    </select>
+                    <label>Size:</label>
+                    <select className="client_select">
+                        <option>Choose any Option</option>
+                        <option>A4(8X12 inch)</option>
+                        <option>A3(12X16 inch)</option>
+                        <option>A2(16X24 inch)</option>
+                    </select>
+                    <label>Frame</label>
+                    <select className="client_select">
+                        <option>Choose any Option</option>
+                        <option>Black</option>
+                        <option>None</option>
+                    </select>
+                    <label>Faces:</label>
+                    <select className="client_select">
+                        <option>Choose any Option</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>2 or More</option>
+                    </select>
+                    <label>Mode of Payment</label>
+                    <select className="client_select">
+                        <option>Choose any Option</option>
+                        <option>Upi</option>
+                        <option>Net Banking</option>
+                        <option>COD</option>
+                    </select>
+                    <span> Upload Image You Want to Sketch </span>
+                    <span style={{height:"5px"}} />
+                    <input
+                        type="file"
+                        name="myImage"
+                        onChange={(event) => {
+                        setSelectedImage(event.target.files[0]);
+                        }}
+                        style={{color:"transparent"}}
+                    />
+                    <div className="next_page">
+                        <Button variant="contained" className="order_button">
+                            <Link to="/cart" className="order_link">Add To Cart</Link>
+                        </Button>
+                        <Button variant="contained" className="order_button">
+                            <Link to="/order" className="order_link">Order Now</Link>
+                        </Button>
+                    </div>
+                </div>
+                <div className="selected_image">
+                        
+                    {selectedImage && (
+                        <div>
+                        <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+                        <br />
+                        <Button className="order_button" onClick={()=>setSelectedImage(null)}>Remove Image</Button>
+                        </div>
+                    )}
+                </div>
+            </div>
+            </>
+        )
+}
+export default PencilSketch;
--- src/Components/CreateOrder/createOdrer.js
+++ src/Components/CreateOrder/createOdrer.js
@@ -1,31 +1,35 @@
 import React, {useState} from "react";
+import "../CreateOrder/order.scss"
 import TopBar from "../Common/Topbar/Topbar";
+import PencilSketch from "./SketchOrderType/PencilSketch";
+import ColorPencilSketch from "./SketchOrderType/ColorPencilSketch";
+import { Tabs, Tab, TabPanel, TabList } from "react-web-tabs";
+import "react-web-tabs/dist/react-web-tabs.css";
 
 function CreateOrder() {
-    const [selectedImage, setSelectedImage] = useState(null);
+    const [defaultTab, setdefaultTab] = useState('Pencil');
+    const handleChange = () =>{
 
+    }
     return (
         <>
              <TopBar />
-             <span> Upload Image You Want to Sketch </span>
-            {selectedImage && (
-                <div>
-                <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
-                <br />
-                <button onClick={()=>setSelectedImage(null)}>Change Image</button>
+             <div className="content">
+                <div className="listing">
+                    <Tabs defaultTab={defaultTab} onChange={handleChange} className="tabs_data">
+                        <TabList className="listTab">
+                            <Tab tabFor="Pencil" className="tabs">Pencil Sketch</Tab>
+                            <Tab tabFor="Color" className="tabs">Color Pencil Sketch</Tab>
+                        </TabList>
+                        <TabPanel tabId="Pencil">
+                                <PencilSketch />
+                        </TabPanel>
+                        <TabPanel tabId="Color">
+                                <ColorPencilSketch />
+                        </TabPanel>
+                    </Tabs>
                 </div>
-            )}
-            <br />
-     
-            <br /> 
-            <input
-                type="file"
-                name="myImage"
-                onChange={(event) => {
-                console.log(event.target.files[0]);
-                setSelectedImage(event.target.files[0]);
-                }}
-            />
+             </div>
         </>
     )
 }
